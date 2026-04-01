ee1b73e23bcabe688e1a661d75d4f26c5c0d2d06
Gert07 <puuramartin@gmail.com>
2026-04-01 12:05:08 +0300

    laen üles

--- Html&Css&JavaScript/JavaScript/ContentChange.js
+++ Html&Css&JavaScript/JavaScript/ContentChange.js
@@ -5,11 +5,20 @@ function loadPage(page) {
       document.getElementById("content").innerHTML = data;
     });
 }
-
+//Main Menu
 function toggleMenu() {
     document.getElementById("menu").classList.toggle("active");
 }
-
+//Second Menu
 function toggleMenu2() {
     document.getElementById("menu2").classList.toggle("active");
+}
+//Submenus
+function toggleSubmenu1() {
+    document.getElementById("submenu1").classList.toggle("active");
+}
+//SubMenu
+function toggleSubmenu(button) {
+  const submenu = button.nextElementSibling;
+  submenu.classList.toggle("active");
 }
