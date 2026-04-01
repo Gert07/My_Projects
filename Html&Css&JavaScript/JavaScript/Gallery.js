f9c9b0e6e6e5a1491657060e342617d27fe9e288
Gert07 <puuramartin@gmail.com>
2026-04-01 21:54:49 +0300

    Gallery needs fixing

---
+++ Html&Css&JavaScript/JavaScript/Gallery.js
@@ -0,0 +1,33 @@
+const searchInput = document.getElementById("search");
+const images = document.querySelectorAll(".gallery img");
+
+searchInput.addEventListener("input", function () {
+    const value = this.value.toLowerCase();
+
+    images.forEach(img => {
+        const altText = img.alt.toLowerCase();
+
+        if(altText.includes(value)) {
+            img.style.display = "block";
+        } else {
+            img.style.display = "none";
+        }
+    });
+});
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.getElementById("close");
+
+// open
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+// close button
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
